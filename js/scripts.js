96aea0f1c4f41745a916a922f92f9f84ee76829a
dmndesigns <80652258+dmndesigns@users.noreply.github.com>
2021-04-11 20:39:01 -0500

    Created pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
-alert('Hello world');
-let favoriteFood = 'Chocolate Mousse';
-document.write (favoriteFood);
+let pokemonList = 
+    [{name: 'Bulbasaur', height: 0.7 , types: ['grass','poison']}, 
+    {name: 'Venonat', height: 1 , types: ['bug','poison']}, {name: 'Xatu', height: 1.5 , types: ['grass','flying']}];
