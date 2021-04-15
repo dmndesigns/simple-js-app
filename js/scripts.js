133e0b15a70a784fd1271cfb2d68e1fe568096d7
dmndesigns <80652258+dmndesigns@users.noreply.github.com>
2021-04-14 21:04:31 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 let pokemonList = 
     [{name: 'Bulbasaur', height: 0.7 , types: ['grass','poison']}, 
-    {name: 'Venonat', height: 1 , types: ['bug','poison']}, {name: 'Xatu', height: 1.5 , types: ['grass','flying']}];
+    {name: 'Venonat', height: 1 , types: ['bug','poison']}, 
+    {name: 'Xatu', height: 1.5 , types: ['grass','flying']
+    }
+];
+
+for (let i = 0; i < pokemonList.length; i++)
+    { document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") ") 
+    }
+
+if (pokemonList.height > 1.1) {
+    document.write('Wow, that\'s big!');
+}
