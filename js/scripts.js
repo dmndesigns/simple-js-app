6b10ced5199b461271e9089d96663bb53b731358
dmndesigns <80652258+dmndesigns@users.noreply.github.com>
2021-04-17 13:38:55 -0500

    insert line break between heights

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,8 @@ let pokemonList =
 //write Pokemon name and height, commenting on tallest
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 1.1) {
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") "+ 'Wow, that\'s big!');}
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") "+ 'Wow, that\'s big!' + "<br>");}
     else { 
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") ") 
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + "<br>") 
     }
 }
