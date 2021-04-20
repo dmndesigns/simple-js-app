7f37029e8a8ecf6122690cdcacf4d63995e8c63a
dmndesigns <80652258+dmndesigns@users.noreply.github.com>
2021-04-20 14:01:07 -0500

    enclose list in function

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,50 @@
 //define characteristics of relevant Pokemon
-let pokemonList = 
-    [{name: 'Bulbasaur', height: 0.7 , types: ['grass','poison']}, 
-    {name: 'Venonat', height: 1 , types: ['bug','poison']}, 
-    {name: 'Xatu', height: 1.5 , types: ['grass','flying']
+var pokemonRepository = (function () {
+    var pokemonList = [
+    {
+        name: 'Bulbasaur', 
+        height: 0.7 , 
+        types: ['grass','poison']
+    }, 
+
+    {
+        name: 'Venonat',
+        height: 1 , 
+        types: ['bug','poison']
+    }, 
+
+    {
+        name: 'Xatu', 
+        height: 1.5 , 
+        types: ['grass','flying']
     }
 ];
 
+function getAll() {
+    return pokemonList;
+} 
+
+function add(pokemon) {
+    pokemonList.push(pokemon);
+}
+
+return {
+    getAll: getAll,
+    add: add
+};
+
+})();
+
+document.write(pokemonRepository.getAll())
+
 //write Pokemon name and height, commenting on tallest
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.1) {
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") "+ 'Wow, that\'s big!' + "<br>");}
-    else { 
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + "<br>") 
+/*getAll.forEach(function(pokeHeight) {
+    if (pokeHeight.height() > 1.1) {   
+    document.write(pokeHeight.name() + " (height: " + pokeHeight.height() + ") " + 'Wow, that\'s big!' + "<br>") 
+    }
+    else {
+        document.write(pokeHeight.name() + " (height: " + pokeHeight.height() + ") " + "<br>")
     }
 }
+)
+*/
